2f2338687688bdb2ec6471c1d47fd8bd4e728380
khuongitptit <khuongitptit@gmail.com>
2020-06-23 19:57:23 +0700

    Updates

---
+++ precache-manifest.cdab3f90ecfefd561ccb5764ca80f638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8feea048495bcd6600010ca896e60ae",
+    "url": "/todolist-react-redux/index.html"
+  },
+  {
+    "revision": "a19f170a1e48bca25524",
+    "url": "/todolist-react-redux/static/css/main.2b73b2e7.chunk.css"
+  },
+  {
+    "revision": "190b3c09acfdea241cdd",
+    "url": "/todolist-react-redux/static/js/2.7af5ab71.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/todolist-react-redux/static/js/2.7af5ab71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a19f170a1e48bca25524",
+    "url": "/todolist-react-redux/static/js/main.2e6a2808.chunk.js"
+  },
+  {
+    "revision": "53ab81f1011d6afa9cab",
+    "url": "/todolist-react-redux/static/js/runtime-main.c32bc4ba.js"
+  }
+]);
